c6db6dbb563aa74c69d56ef7b0c8de45c81902c8
desyasyasha <13517037@std.stei.itb.ac.id>
2020-12-14 20:42:50 +0700

    inisiasi crud question

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
+var dotenv = require('dotenv').config() //supaya .env nya bisa jalan
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var questionRouter = require('./routes/questionRouter');
 
 var app = express();
+
+//url mongodbserver
+var url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-shard-00-00.x0xn8.mongodb.net:27017,cluster0-shard-00-01.x0xn8.mongodb.net:27017,cluster0-shard-00-02.x0xn8.mongodb.net:27017/${process.env.DATABASE}?ssl=true&replicaSet=atlas-frowu0-shard-0&authSource=admin&retryWrites=true&w=majority`;
+var connect = mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
+
+connect.then((db)=>{
+  console.log('berhasil');
+}, (err)=>{
+  console.log(err);
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/question', questionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
