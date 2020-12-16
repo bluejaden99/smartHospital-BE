c67ba3e1880581d3f8c9b6bc68d68d16ef6aa36c
desyasyasha <13517037@std.stei.itb.ac.id>
2020-12-16 10:21:34 +0700

    solve conflict and merge

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var news = require('./news_api.js');
+var news = require('../news_api');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
     const news = await news.articles;
     console.log(news);
     res.render('news', {news});
