ef48093f36536e12c1c965eebb94cd7f2adf4eda
Yunanda Pratama <yunandapratama21@gmail.com>
2020-12-17 12:49:55 +0700

    Update CRUD rapidTestRouter

--- routes/rapidTestRouter.js
+++ routes/rapidTestRouter.js
@@ -10,43 +10,74 @@ rapidTestRouter.use(bodyParser.json());
 rapidTestRouter.route('/')
   .get((req, res, next)=>{
     FormRapid.find().then((formRapid)=>{
-      res.status = 200;
-      res.setHeader('Content-type', 'application/json');
-      res.json(formRapid);
-    })
+        res.status = 200;
+        res.setHeader('Content-type', 'application/json');
+        res.json(formRapid);
+    });
   })
 
   .post((req, res, next)=>{
     FormRapid.create(req.body).then((formRapid)=>{
-      console.log("Insert Success")
+    console.log("Insert Success")
       res.status = 200;
       res.setHeader('Content-type', 'application/json');
       res.json(formRapid);
-    })
+    });
   })
 
   .put((req, res, next) => {
-    res.statusCode = 403;
-    res.end('Not Support for Updating');
-  })
+    FormRapid.findByIdAndUpdate({}, {
+      $set: req.body
+    }, {new: true}).then ((rapid)=> {
+      res.status = 200;
+      res.setHeader('Content-type', 'application/json');
+      res.json(rapid);
+  });
+})
 
   .delete((req, res, next) => {
-    FormRapid.remove({}).then((formRapid) => {
+    FormRapid.remove({})
+    .then((rapid) => {
         res.status = 200;
         res.setHeader('Content-type', 'application/json');
-        res.json(formRapid);
+        res.json(rapid);
       });
   });
 
 rapidTestRouter.route('/:rapidId')
+  .get((req, res, next) => {
+    console.log(req.params.dishId);
+    FormRapid.findById(req.params.rapidId)
+    .then((rapid) => {
+        res.status = 200;
+        res.setHeader('Content-type', 'application/json');
+        res.json(rapid);
+    });
+})
+  
+  .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('Not Support for Input Data');
+    })
+
+  .put((req, res, next) => {
+    FormRapid.findByIdAndUpdate(req.params.rapidId, {
+    $set: req.body
+  }, { new: true })
+    .then((rapid) => {
+      res.status = 200;
+      res.setHeader('Content-type', 'application/json');
+      res.json(rapid);
+    });
+})
 
   .delete((req, res, next) => {
     FormRapid.findByIdAndRemove(req.params.rapidId)
-      .then((formRapid) => {
+      .then(() => {
         res.status = 200;
         res.setHeader('Content-type', 'application/json');
-        res.json(formRapid);
-      });
-  });
+        res.end ('Remove Success');
+    });
+});
 
 module.exports = rapidTestRouter;
