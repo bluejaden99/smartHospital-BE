e5fe615c0a5311ba38f578ad06f0fa13e93cfd0b
desyasyasha <13517037@std.stei.itb.ac.id>
2020-12-19 11:58:01 +0700

    connect nigML

--- service/cek-covid.js
+++ service/cek-covid.js
@@ -1,3 +1,5 @@
+const { connection } = require('mongoose');
+
 const s1 = "Anda kemungkinan terkena virus corona. Hubingi hotline RS Soebandi No telpon (0331) 487441 untuk pemeriksaan lebih lanjut. Lakukan social distance, gunakan masker, cuci tangan dengan teratur, dan lakukan isolasi diri di rumah"
 const s2 = "Ada kemungkinan anda terkena virus corona. Lakukan social distance, gunakan masker, cuci tangan dengan teratur, lakukan isolasi diri di rumah. Jika terjadi perubahan dalam kurun waktu 14 hari, muncul gejala demam > 38°C, batuk, pilek dan sesak tidak mereda maka periksakan diri ke puskesmas atau dokter terdekat"
 const s3 = "Anda tidak terkena virus corona. Jaga kesehatan dan tetap waspada dengan melakukan social distance atau tetap berada di rumah, cuci tangan secara teratur, minum vitamin, istirahat yang cukup, dan makan makanan bergizi"
@@ -26,4 +28,15 @@ function cek_covid(data){
   else return cek_kondisi(data)
 };
 
-module.exports = cek_covid
+function cek_covid_asso(data){
+  console.log("masuk bigml")
+  var bigml = require('bigml');
+  var connection = new bigml.BigML('bluejaden99', '2eb0296ff89bdc9ca84e809412405db800f1654c')  
+  var localModel = new bigml.LocalModel('model/5fdd48492fb31c5604000ac0', connection);
+    localModel.predict({'petal length': 8, 'sepal-width' :  8},function(error, prediction) {console.log(prediction)}); 
+}
+
+module.exports = {
+  cek_covid,
+  cek_covid_asso
+}
